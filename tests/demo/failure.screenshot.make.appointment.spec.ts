9a071ac62807b9e02efb02ae9d84ba3b2b64578c
Daniele <danielemarinelli1387@gmail.com>
2026-08-24 10:48:04 +0200

    added a test for capturing screenshots

---
+++ tests/demo/failure.screenshot.make.appointment.spec.ts
@@ -0,0 +1,73 @@
+import { test, expect } from "@playwright/test";
+import { log } from "../helpers/logger.js";
+
+test.describe("Make Appointment", () => {
+    test.beforeEach("Login with valid creds", async ({ page }, testInfo) => {
+        // 1. Launch URL and assert title and header
+
+        // Get the URL from config file
+        const envConfig = testInfo.project.use as any;
+
+        // Custom logs
+        await log("info", `Launching the web app in ${envConfig.envName}`)
+
+        await page.goto("https://katalon-demo-cura.herokuapp.com/");
+        await expect(page).toHaveTitle("CURA Healthcare Service");
+        await expect(page.locator("//h1")).toHaveText("CURA Healthcare Service");
+
+        // 2. Click on the Make Appointment
+        await page.getByRole("link", { name: "Make Appointment" }).click();
+        await expect(page.getByText("Please login to make")).toBeVisible();
+
+        // Successful login
+        await page.getByLabel("Username").fill("John Doe");
+        await page.getByLabel("Password").fill("ThisIsNotAPassword");
+        await page.getByRole("button", { name: "Login" }).click();
+
+        // ADD custom screenshot at this point to capture
+        let fullScreenshotPage = await page.screenshot({fullPage: true});
+        await log("info", "Screenshot captured on failure");
+        await testInfo.attach("fullPageScreenshot", {
+            body: fullScreenshotPage,
+            contentType: "image/png"
+        });
+
+        // Assert a text
+        await expect(page.locator("h2")).toContainText("Make Appointment");
+        
+    });
+
+
+    test("Should make an appointment with non-default values", async ({ page }, testInfo) => {
+        
+        // console.log(`>> Current config \n: ${JSON.stringify(testInfo.config)}`);
+        
+        // Dropdown
+        await page.getByLabel("Facility").selectOption("Hongkong CURA Healthcare Center");
+
+        // Checkbox
+        await page.getByText("Apply for hospital readmission").click();
+
+        // Radio button
+        await page.getByText("Medicaid").click();
+
+        // Date input box
+        await page.getByRole("textbox", { name: "Visit Date (Required)" }).click();
+        await page.getByRole("textbox", { name: "Visit Date (Required)" }).fill("05/10/2027");
+        // commenting line below will fail the test and capture a screenshot of the failure
+        //await page.getByRole("textbox", { name: "Visit Date (Required)" }).press("Enter");
+
+        // Multi-line comments input box
+        await page.getByRole("textbox", { name: "Comment" }).click();
+        await page.getByRole("textbox", { name: "Comment" }).fill("This is a multi-line comments\ncaptured by Playwright codegen!");
+
+        // Button
+        await page.getByRole("button", { name: "Book Appointment" }).click();
+
+        // Assertion
+        await expect(page.locator("h2")).toContainText("Appointment Confirmation");
+        await expect(page.getByRole("link", { name: "Go to Homepage" })).toBeVisible();
+    });
+
+    // More tests go here ...
+});
