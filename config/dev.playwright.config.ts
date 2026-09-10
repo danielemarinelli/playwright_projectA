990efaec8df1eaf6121fa8fe4d57b12d40ef9a56
Daniele <danielemarinelli1387@gmail.com>
2026-09-10 19:17:24 +0200

    dev env config

---
+++ config/dev.playwright.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig, devices } from "@playwright/test";
+import { baseConfig } from "../playwright.config.ts";
+import { EnvConfig } from "../tests/helpers/config-fixtures.ts";
+import path from "path";
+
+// stores environment specific configuration values for the tests
+
+console.log(`---LOADING DEV ENV SETTINGS---`);
+
+export default defineConfig<EnvConfig>({
+  ...baseConfig, // Loads all existing config values...
+  testDir: path.resolve(process.cwd(), "./tests"),
+  use: {
+    ...baseConfig.use, // Loading the existing use object of playwright.config.ts and the below
+    envName: "dev",
+    appURL: "https://dev_katalon-demo-cura.herokuapp.com/",  // now no hardcoded values in the tests, we can use this env variable instead
+    nopCommerceWeb: "https://dev_admin-demo.nopcommerce.com",
+    apiURL: "https://dev_reqres.in/api",
+    dbConfig: {
+      server: "",
+      dbname: "",
+      connnectionStr: "",
+    },
+  },
+});
