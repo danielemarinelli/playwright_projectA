483a017be6ebe9330a4dee24a98c9e7a9fe4da04
Daniele <danielemarinelli1387@gmail.com>
2026-09-10 19:20:51 +0200

    no more hardcoded information

--- tests/functional/better-login.spec.ts
+++ tests/functional/better-login.spec.ts
@@ -1,11 +1,16 @@
 import { test, expect } from '@playwright/test';
+import { log } from "../helpers/logger.js";
 
 test.describe("Login functionality",() => {
 
   // hooks - this runs before every test
-  test.beforeEach("", async ({page}) => {
+  test.beforeEach("", async ({page}, testInfo) => {
+  const envConfig = testInfo.project.use as any;
+  await log("info", `Launching the web app in ${envConfig.envName}`)  // Custom logs
+            // Get URL file from config file (test.playwright.config.ts)    best practice!
+  await page.goto(envConfig.appURL);
       // launch URL and assert title and header
-  await page.goto('https://katalon-demo-cura.herokuapp.com/');
+  //await page.goto('https://katalon-demo-cura.herokuapp.com/');
   await expect(page).toHaveTitle("CURA Healthcare Service");
   await expect(page.locator('//h1')).toHaveText('CURA Healthcare Service')
     // click on the make appoitment
@@ -18,9 +23,9 @@ test('Should Login Successful', async ({ page }) => {
   
     // login
   await page.getByLabel('Username').click();
-  await page.getByLabel('Username').fill('John Doe');
+  await page.getByLabel('Username').fill(process.env.TEST_USER_NAME);
   await page.getByLabel('Password').click();
-  await page.getByLabel('Password').fill('ThisIsNotAPassword');
+  await page.getByLabel('Password').fill(process.env.TEST_USER_PASSWORD);
   await page.getByRole('button', { name: 'Login' }).click();
     //assert
   await expect(page.getByRole('heading', { name: 'Make Appointment' })).toBeVisible();
