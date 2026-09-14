482129a25b7e1b3209abaa9a3e04d16220ec8e9c
Daniele <danielemarinelli1387@gmail.com>
2026-09-14 12:41:53 +0200

    added new test

--- tests/demo/test1.spec.ts
+++ tests/demo/test1.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from "@playwright/test";
 import constants_data from "../../data/constants.json";
+import { log } from "../helpers/logger.js";
 
 test("Should load home page with correct title", async ({ page }) => {
     // Go to the home page
@@ -25,7 +26,26 @@ test("Should demo parallel execution second", {tag : '@demo'}, async ({ page, br
     await page.goto("https://www.ansa.it");
 });
 
-test.only("Should demo constants type data", async ({ page, browserName }, testInfo) => {
+test("Should demo constants type data", async ({ page, browserName }, testInfo) => {
     console.log(`>> Constant SUCCESS: ${JSON.stringify(constants_data.STATUSCODES.SUCCESS)}`);
     console.log(`>> Constant data: ${JSON.stringify(constants_data.STATUSCODES)}`);
+});
+
+test.only("Should demo click action with PageObjects", async ({ page, browserName }, testInfo) => {
+            // default action
+        //await page.goto("https://katalon-demo-cura.herokuapp.com/");
+        //await expect(page).toHaveTitle("CURA Healthcare Service");
+        let ele = page.getByRole("link", { name: "Make Appointment" });
+        //await ele.click();
+
+        // Page Object action
+        await page.goto("https://katalon-demo-cura.herokuapp.com/");
+    
+    try {
+        await expect(ele).toBeVisible({ timeout: 10_000 }); // Custom timeout: Default - 5 seconds
+        await ele.click();
+    } catch (error) {
+        await log("error", `Failed to click element: ${ele.toString()}, original error: ${error}`);
+        throw error;
+    }
 });
