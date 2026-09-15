1372ebb959b0d4a4954f53dde82331b3ff0d8cb0
Daniele <danielemarinelli1387@gmail.com>
2026-09-15 18:52:45 +0200

    refactor e2e with search customer feature

--- tests/e2e/nopcommerce.e2e.spec.ts
+++ tests/e2e/nopcommerce.e2e.spec.ts
@@ -1,17 +1,36 @@
 import { test, expect } from "@playwright/test";
 import { log } from "../helpers/logger.js";
 import HomePage from "../page-objects/nopcommerce.home.page.js";
+import CustomerList from "../page-objects/nopcommerce.customer.list.page.js";
 
+test.describe("nopCommerce E2E Customer Search", () => {
 
-test("Should login to nopCommerce app", async ({ page, }, testInfo) => {
-    // Env config from test.playwright.config.ts
-    const envConfig = testInfo.project.use as any;
-    // create object of HomePage class
-    const homePage = new HomePage(page);  
-    // Login to nopCommerce app
-    await homePage.loginToNopCommerceApp(
-        envConfig.nopCommerceWeb, 
-        process.env.NOP_COMMERCE_TEST_USERNAME, 
-        process.env.NOP_COMMERCE_TEST_PASSWORD
-    );
-}); 
+    test("TC001_E2E: Customer_Search", async ({ page, }, testInfo) => {
+        // Env config from test.playwright.config.ts
+        const envConfig = testInfo.project.use as any;
+          
+        // Login to nopCommerce app
+        const homePage = new HomePage(page);
+        await homePage.loginToNopCommerceApp(
+            envConfig.nopCommerceWeb, 
+            process.env.NOP_COMMERCE_TEST_USERNAME, 
+            process.env.NOP_COMMERCE_TEST_PASSWORD
+        );
+
+        // Search for a customer
+        let USER_DATA = {
+            firstname: "Alex",
+            lastname: "Thomas"
+        }
+        await log("info", `Searching for a customer with firstname: ${USER_DATA.firstname} and lastname: ${USER_DATA.lastname}...`);
+        const customerListPage = new CustomerList(page);
+        await customerListPage.goToCustomerListPage(`${envConfig.nopCommerceWeb}/Admin/Customer/List`);
+        let customerNotFound = await customerListPage.searchAndConfirmUser(USER_DATA.firstname, USER_DATA.lastname);
+        
+        if (customerNotFound) {
+            await log("warn", `Customer with firstname: ${USER_DATA.firstname} and lastname: ${USER_DATA.lastname} NOT found!!`);
+        }else {
+            await log("info", `Customer with firstname: ${USER_DATA.firstname} and lastname: ${USER_DATA.lastname} found!!`);
+        }
+    }); 
+})
